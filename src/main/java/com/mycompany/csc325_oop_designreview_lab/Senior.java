a391093d33227abbf08be1e46b2ca76f49143c93
jaleve456 <jaleve456@gmail.com>
2025-07-16 21:22:00 -0400

    Update comments for Senior.java

--- src/main/java/com/mycompany/csc325_oop_designreview_lab/Senior.java
+++ src/main/java/com/mycompany/csc325_oop_designreview_lab/Senior.java
@@ -1,15 +1,15 @@
 package com.mycompany.csc325_oop_designreview_lab;
 
-public class Senior extends Student {
-    private int seniorCredits;
+public class Senior extends Student { //inherits traits from Student class
+    private int seniorCredits; //sets specfic credit attribute for Senior class
     private String address;
 
     public Senior (String name, short age, double GPA, int seniorCredits) { //constructor method for Senior class
-        super(name,age,GPA);
-        if (seniorCredits >= 85) {
+        super(name,age,GPA); //gets attributes for Student superclass
+        if (seniorCredits >= 85) { //sets a minimum value of 85 to seniorCredits 
             this.seniorCredits = seniorCredits;
         } else {
-            throw new IllegalArgumentException("Senior must have at least 85 credits");
+            throw new IllegalArgumentException("Senior must have at least 85 credits"); //throws an exception is seniorCredits does not meet the minimum requirement of 85 credits
         }
     }
 
@@ -18,23 +18,24 @@ public class Senior extends Student {
     }
 
     public void setSeniorCredits(int seniorCredits) { //setter method for credits
-        if (seniorCredits >= 85) {
+        if (seniorCredits >= 85) { //sets a minimum value of 85 to seniorCredits 
             this.seniorCredits = seniorCredits;
         } else {
-            throw new IllegalArgumentException("Senior must have at least 85 credits");
+            throw new IllegalArgumentException("Senior must have at least 85 credits"); //throws an exception is seniorCredits does not meet the minimum requirement of 85 credits
+        }
         }
     }
-    @Override
+    @Override //overrides abstract method in Human class
     public String toString() {
-        return  "Senior: Name = '" + getName() + "', Age = " + getAge() + ", GPA = " + getGPA() + ", Credits = " + getSeniorCredits();
+        return  "Senior: Name = '" + getName() + "', Age = " + getAge() + ", GPA = " + getGPA() + ", Credits = " + getSeniorCredits(); //returns this format when Senior object is called in MainClass.java
     }
 
-    @Override
+    @Override //overrides abstract method in Human class
     public String getAddress() {
         return address;
     }
 
-    @Override
+    @Override //overrides abstract method in Human class
     public void setAddress(String address) {
         this.address = address;
     }
